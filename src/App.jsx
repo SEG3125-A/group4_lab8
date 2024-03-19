be26c67c54fc583aabe2437db737cdf93124b5e4
fatima7141 <fabdali07@gmail.com>
2024-03-19 16:00:29 -0400

    small adjustments

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,14 @@
 import { TbActivityHeartbeat } from "react-icons/tb";
+import Navbar from "./Components/Navbar";
+
 function App() {
   return (
     <div>
       <header>
-        <div>
-          <h1>
-            Health Up <TbActivityHeartbeat color="40deg" />
-          </h1>
-
-          {/* I deleted this logo for now
-          <img src="src/assets/Logo1.jpg" alt="Stylized atom" />*/}
-        </div>
-        <p>
-          Focus on the 3 main aspects of your health: mental health, fitness,
-          diet !
-        </p>
+        <Navbar />
       </header>
-      <main>
-        <h2>Login</h2>
-      </main>
     </div>
   );
 }
-//!!
 
 export default App;
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,25 @@
+import { TbActivityHeartbeat } from "react-icons/tb";
+
+export default function Navbar() {
+  return (
+    <nav className="nav">
+      <b href="/" className="site-title">
+        Health Up <TbActivityHeartbeat color="#44a9c0" />
+      </b>
+      <ul>
+        <li>
+          <a href="/Dashboard">Dashboard</a>
+        </li>
+        <li>
+          <a href="/FitnessCoaches">Fitness Coaches</a>
+        </li>
+        <li>
+          <a href="/MentalHealthSpecialists">Mental Health Specialists</a>
+        </li>
+        <li>
+          <a href="/Nutritionists">Nutritionists</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
